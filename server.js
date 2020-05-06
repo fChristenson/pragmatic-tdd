0e6c219feb3e07bd0061fe9e22fdf034269a42dc
fChristenson <fredrik.christenson@gmail.com>
2020-05-06 20:54:51 +0200

    wp

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./src/app");
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log("Running server on port:", port);
-  console.log("--------------------------");
-});
